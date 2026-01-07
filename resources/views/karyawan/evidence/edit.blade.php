92ad1603f068561f5a66145dff1addbf05d6a7fd
arfaniakbar <akbarhansem01@gmail.com>
2026-01-07 16:18:26 +0800

    fix: restore image display in evidence edit form

--- resources/views/karyawan/evidence/edit.blade.php
+++ resources/views/karyawan/evidence/edit.blade.php
@@ -176,8 +176,7 @@
                             @endphp
                             @if($path)
                                 <div class="file-preview-item" data-file-index="{{ $index }}">
-                                    {{-- <img src="{{ Storage::url($path) }}" alt="{{ $caption }}"> --}}
-                                    <div style="padding: 20px; text-align: center; background: #eee;">Image Placeholder</div>
+                                    <img src="{{ Storage::url($path) }}" alt="{{ $caption }}">
                                     <div class="file-info">{{ $caption }}</div>
                                     <button type="button" class="delete-btn" onclick="deleteFile({{ $index }})">
                                         <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
